c3a3cf09d75b39d633047dc511b592306a8229f8
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-01-28 01:04:34 +0530

    Day 4 Assignment Submission

---
+++ Day 4 Morning Assignment by Manoj.K - 27 Jan 2022/Day4Project1(factorial-functions)/Day4Project1(factorial-functions)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// This is A C# Code used to find the Factorial of Numbers By using Functions concept.
+// Code Developed By Manoj Karnatapu - 27 Jan, 2022
+
+namespace Day4Project1_factorial_functions_
+{
+    internal class Program
+    {
+        public static void PrintOutput(int n)
+        {
+            Console.WriteLine("Factorial of {0} = {1}",n, Factorial(n));
+        }
+
+        public static int Factorial(int n)
+        {
+            int fact = 1;
+
+            for(int i = 1; i <= n;i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            int n = 4, n1 = 6, n2 = 3;
+
+            PrintOutput(n);
+            PrintOutput(n1);
+            PrintOutput(n2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 4 Morning Assignment by Manoj.K - 27 Jan 2022/Day4Project2(aPowerb-functions)/Day4Project2(aPowerb-functions)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// This is a C# Code to find the Power of a number, Using Functions concept
+// This Code is Developed by Manoj Karnatapu, on 27 Jan 2022.
+
+namespace Day4Project2_aPowerb_functions_
+{
+    internal class Program
+    {
+        public static void PrintOutput(int a, int b)
+        {
+            Console.WriteLine("{0} power {1} = {2}", a, b, Power(a, b));
+        }
+        public static int Power(int a, int b)
+        {
+            int pow = 1;
+            for (int i = 1; i <= b; i++)
+                pow = pow * a;
+            
+            return pow;
+        }
+        static void Main(string[] args)
+        {
+            int a1 = 4, b1 = 3, a2 = 5, b2 = 4;
+
+            PrintOutput(a1, b1);
+            PrintOutput(a2, b2);
+            
+            Console.ReadLine();
+        }
+    }
+}
