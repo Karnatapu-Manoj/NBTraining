e5b4186ad91eb0403bf8e8e5132b217440352b0e
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-01-31 12:10:50 +0530

    Declare ArrayList with 5_values & Find Sum

---
+++ Day 6 Morning Assignment by Manoj.Karnatapu - 31 Jan 2022/Day6Project1/Day6Project1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+// Author: Manoj.Karnatapu
+// Purpose : To Declare ArrayList, assign some Values & Find Sum/
+
+namespace Day6Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList data = new ArrayList();
+
+            int sum = 0;
+
+            data.Add(10);
+            data.Add(20);
+            data.Add(30);
+            data.Add(40);
+            data.Add(50);
+
+            foreach(var d in data)
+            {
+                sum = sum + (int)d;
+            }
+
+            Console.WriteLine("The Sum of Array List items is : {0}", sum);
+
+            Console.ReadLine();
+
+        }
+    }
+}
