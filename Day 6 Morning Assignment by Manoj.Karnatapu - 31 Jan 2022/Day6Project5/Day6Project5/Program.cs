e224c16d94c6a0a5d9de251d62add6cdf6651cb1
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-01-31 16:26:57 +0530

    Implicit & Explicit Type Conversion

---
+++ Day 6 Morning Assignment by Manoj.Karnatapu - 31 Jan 2022/Day6Project5/Day6Project5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day6Project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+            // Type Casting short to int
+
+            short myShort = 10;
+            int myInt = myShort;
+
+            Console.WriteLine("Implicit Conversion of short to int is : {0}",myInt);
+
+            // Explicit Conversion
+            // Casting double to int
+
+            double myDouble = 13.37;
+            int myNewInt = (int)myDouble;
+            Console.WriteLine("Explicit Conversion of double to int is : {0}",myNewInt);
+            Console.ReadLine();
+        }
+    }
+}
