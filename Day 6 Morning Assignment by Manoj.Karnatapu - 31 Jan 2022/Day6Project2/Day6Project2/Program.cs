0904d64ab37ce978300ad82c4aae4187627aba21
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-01-31 13:02:58 +0530

    Declare a List<int> with 5_Values & Find Sum

---
+++ Day 6 Morning Assignment by Manoj.Karnatapu - 31 Jan 2022/Day6Project2/Day6Project2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Author : Manoj.Karnatapu
+//Purpose : Declare a List<int> with 5 values & find Sum
+
+namespace Day6Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = new List<int>();
+
+            int sum = 0;
+
+            data.Add(5);
+            data.Add(10);
+            data.Add(20);
+            data.Add(30);
+            data.Add(50);
+
+            foreach(int i in data)
+            {
+                sum = sum + i;
+            }
+
+            Console.WriteLine("The Sum Of given List items is : {0}", sum);
+
+            Console.ReadLine();
+        }
+    }
+}
