6a5ed1a0cda7ce99ecfa7c1e32fe6a56cfeaea15
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-01 17:38:09 +0530

    Create.Employee.Class & initialize values while Object Creation

---
+++ Day 7 Morning Assignment by Manoj.Karnatapu - 1 Feb 2022/Day7Project3/Day7Project3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Create Employee Class with 3 Public variables. Crete object and initialize with values while creating object and print the values.
+
+namespace Day7Project3
+{
+    class Employee
+    {
+        public int employeeId;
+        public string employeeName;
+        public int employeeAge;
+        public int employeeSalary;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() {employeeId = 310596, employeeName = "Manoj.Karnatapu", employeeAge = 25, 
+             employeeSalary = 35000};
+
+            Console.WriteLine($"\n\t Employee Id = {emp.employeeId},\n\t Employee Name = {emp.employeeName}," +
+                $"\n\t Employee Age = {emp.employeeAge},\n\t Employee Salary = {emp.employeeSalary}");
+
+            Console.ReadLine();
+        }
+    }
+}
