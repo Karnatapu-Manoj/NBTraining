a090cd8c2662d22ffee622b8df3aabd89609249f
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-04 15:14:40 +0530

    C# Code,Method Overriding Using new KeyWord

---
+++ Day 10 Morning Assignment by Manoj.Karnatapu - 4 Feb 2022/Day10Project4/Day10Project4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Write a C# Code for Method Overriding Using new KeyWord.
+
+// for Reference, check Day10Project4 in the same Repository. 
+
+namespace Day10Project4
+{
+    class EnglishGreetings
+    {
+        /// <summary>
+        /// This is a Hi Method, To Greetings Hi
+        /// </summary>
+        public void PrintHi()
+        {
+            Console.WriteLine("Hi");
+        }
+        /// <summary>
+        /// This is a Hello Method, To Greetings Hello
+        /// </summary>
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        /// <summary>
+        /// This is a Good Morning, To Greeting Good Morning.
+        /// </summary>
+        public void PrintGM()
+        {
+            Console.WriteLine("Good Morning");
+        }
+    }
+
+    class TeluguGreetings : EnglishGreetings
+    {
+        public new void PrintGM()
+        {
+            Console.WriteLine("Subhodhayam");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TeluguGreetings obj = new TeluguGreetings();
+
+            Console.WriteLine("\n --------- Method Overriding Using new KEY Word ---------\n");
+            obj.PrintHi();
+            obj.PrintHello();
+            obj.PrintGM();
+
+            Console.ReadLine();
+        }
+    }
+}
