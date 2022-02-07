906532d01e57bd7275873b9f19b69deb5979d1a1
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-07 20:55:23 +0530

    Employee Class Using only Properties

--- Day 11 Assignment by Manoj.Karnatapu - 7 Feb 2022/Day11Project3/Day11Project3/Program.cs
+++ Day 11 Assignment by Manoj.Karnatapu - 7 Feb 2022/Day11Project3/Day11Project3/Program.cs
@@ -8,33 +8,18 @@ namespace Day11Project3
 {
     class Employee
     {
-        private int id;
-        private string name;
-        private string designation;
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-        public string Name
-        {
-            get => name;
-            set { name = value; }
-        }
-        public string Designation
-        {
-            // Setting only Write Only Property
-            set { designation = value; }
-        }
-        public int Salary
-        {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        
+        public int Salary {
             get
             {
-                if (designation == "M")
+                if (Designation == "M")
                     return 90000;
-                else if (designation == "HR")
+                else if (Designation == "HR")
                     return 50000;
-                else if (designation == "TL")
+                else if (Designation == "TL")
                     return 75000;
                 else
                     return 30000;
@@ -50,24 +35,29 @@ namespace Day11Project3
             emp.Id = 100;
             emp.Name = "Mohan Sir";
             emp.Designation = "M";
+            //emp.Salary = (emp.Designation == "M") ? 90000 : 60000;
+           
             Console.WriteLine($"\n {emp.Id}\t {emp.Name}\t {emp.Salary}");
 
             Employee emp1 = new Employee();
             emp1.Id = 101;
             emp1.Name = "J.K";
             emp1.Designation = "TL";
+            
             Console.WriteLine($"\n {emp1.Id}\t {emp1.Name}\t\t {emp1.Salary}");
 
             Employee emp2 = new Employee();
             emp2.Id = 102;
             emp2.Name = "Durga Prasad";
             emp2.Designation = "HR";
+            
             Console.WriteLine($"\n {emp2.Id}\t {emp2.Name}\t {emp2.Salary}");
 
             Employee emp3 = new Employee();
             emp3.Id = 103;
             emp3.Name = "Manoj";
             emp3.Designation = "S";
+            
             Console.WriteLine($"\n {emp3.Id}\t {emp3.Name}\t\t {emp3.Salary}");
 
             Console.ReadLine();
