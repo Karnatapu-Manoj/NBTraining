5b50f68e76060c9a1c18aeb73d33f24bac51cc1e
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-09 15:25:11 +0530

    Declare 2D array(3x2) initialize and print using nested loops

---
+++ Day 13 Assignment by Manoj.Karnatapu - 9 Feb 2022/Day13Project2/Day13Project2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Declaring (3x2) 2-D Array in the same line while declaring & Printing the Values using nested for Loops.
+
+// For Reference, check Day13Project2 in the same Repository.
+namespace Day13Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // initializing & Declaring 2-D Array
+            int[,] data = new int[,] { { 5, 6 }, { 7, 8 }, { 9, 10 } };
+
+            // printing the 2-D Array 
+            Console.WriteLine("\n ----*** 2-D Array Representation for declaring in same line ****---\n");
+            for (int i=0; i < 2;i++)
+            {
+                for(int j=0; j < 2;j++)
+                {
+                    Console.Write(data[i,j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
