68aef6f571dc0ab86cfd44f0e1da11db0bc89f2d
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-09 15:33:50 +0530

    Declare Jagged Arrays & Print Output

---
+++ Day 13 Assignment by Manoj.Karnatapu - 9 Feb 2022/Day13Project7/Day13Project7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Declaring a jagged array & Printing the value.
+
+// For Reference, Check Day13Project7 in the same repository.
+
+namespace Day13Project7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring Jagged Array, and initializing.
+            char[][] names = new char[4][];
+            names[0] = new char[] {'M', 'a', 'n','o', 'j'};
+            names[1] = new char[] { 'P', 'a', 'v', 'a', 'n' };
+            names[2] = new char[] { 'V', 'i', 'h', 'a', 'r' };
+            names[3] = new char[] { 'V', 'i', 'n', 'a', 'y' };
+
+            // Printing The Jagged Array Values.
+            Console.WriteLine("\n --------*** Jagged Array Of Names ***--------\n");
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < names[i].Length; j++)
+                {
+                    Console.Write(names[i][j]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
