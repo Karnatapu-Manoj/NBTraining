8112cea3ace0ca6c1135eec18985726cbe967fc2
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-09 15:29:51 +0530

    Declare 2D Array, Read & Print Array

---
+++ Day 13 Assignment by Manoj.Karnatapu - 9 Feb 2022/Day13Project4/Day13Project4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+// Author : Manoj.Karnatapu
+// Purpose : Declaring (2x2) 2-D Array, Read values from user & Print the Array.
+
+// For Reference, check Day13Project4 in the same Repository.
+namespace Day13Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[2, 2];
+
+            // Reading Array Items from the user
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    Console.Write($"\n Enter the array item at ({i},{j}) :  ");
+                    data[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // Printing the Array Values into the Console.
+            Console.WriteLine("\n Given Array is : \n");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("\t" + data[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
