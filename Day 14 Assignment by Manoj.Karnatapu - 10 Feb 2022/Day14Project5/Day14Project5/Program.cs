a5900128a40229c8bc622b377089d58440d2e04b
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-10 22:36:50 +0530

    printing from 1-30, skipping divisible by 3

---
+++ Day 14 Assignment by Manoj.Karnatapu - 10 Feb 2022/Day14Project5/Day14Project5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Printing Numbers from 1 to 30 by skipping, the numbers divisible by 3.
+
+// for Reference, Check Day14Project5 in the same repository.
+
+namespace Day14Project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The numbers from 1 -30 by skipping divisible by 3 are   :: \n");
+            for (int i = 0;i<=30;i++)
+            {
+                if (i % 3 == 0)
+                    continue;
+
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n");
+            Console.ReadLine();
+        }
+    }
+}
