f1fd3da08b520500174236a265fe1e3eb2fd8fac
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-10 22:30:19 +0530

    Sealed Class Illustration Example

---
+++ Day 14 Assignment by Manoj.Karnatapu - 10 Feb 2022/Day14Project1/Day14Project1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+// Author : Manoj.Karnatapu
+// Purpose : Sealed Class Illustration Example Code.
+
+// For Reference, Check Day14Project1 in the same Repository.
+namespace Day14Project1
+{
+    sealed class Police
+    {
+        public string GetSecret()
+        {
+            return "Encounter order for, Khaide No.420";
+        }
+    }
+    class Khaides : Police // Bcz, sealed class cannot be inherited or can't be derived.
+    {
+        public void Crime()
+        {
+            // Todo
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Khaides k = new Khaides();
+            Console.WriteLine(k.GetSecret());
+
+            Console.ReadLine();
+        }
+    }
+}
