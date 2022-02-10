03eec6702306aa192f34a3e7de64cbd28396321b
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-10 22:35:31 +0530

    Find Prime or Not Using break;

---
+++ Day 14 Assignment by Manoj.Karnatapu - 10 Feb 2022/Day14Project4/Day14Project4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Write a C# Code for, Prime Or Not using Break statement.
+
+// for Reference, check Day14Project4 in the same Repository.
+
+namespace Day14Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Enter any Number to Find Is Prime Or Not :   ");
+            n = int.Parse(Console.ReadLine());
+
+            for(i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    break;
+            }
+            if(i == n)
+                Console.WriteLine("\n Yes, {0} is a Prime Number", n);
+            else
+                Console.WriteLine("\n No, {0} is Not a Prime Number",n);
+
+            Console.ReadLine();
+        }
+    }
+}
