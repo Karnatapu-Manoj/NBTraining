7d372085e4008774924556baab0c199a931692c5
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-10 22:37:56 +0530

    First number after 1000 which is divisible by 97

---
+++ Day 14 Assignment by Manoj.Karnatapu - 10 Feb 2022/Day14Project6/Day14Project6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// Author : Manoj.Karnatapu
+// Purpose : Find the first number after 1000 which is divisible by 97
+
+// for reference, Check Day14Project6 in the same Repository.
+namespace Day14Project6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i <= 1097; i++)
+            {
+                if ( i % 97 == 0 )
+                {
+                    Console.WriteLine("\nThe First Number after 1000 which is divisible by 97 is :  {0}",i);
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
