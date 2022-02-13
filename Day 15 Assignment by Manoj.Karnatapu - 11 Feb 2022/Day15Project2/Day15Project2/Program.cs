2cd5444f5fb30ea2ca924be7b38c082afda9130b
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-13 22:07:45 +0530

    Backing-Up files using Task Schedular

---
+++ Day 15 Assignment by Manoj.Karnatapu - 11 Feb 2022/Day15Project2/Day15Project2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+// Author : Manoj.Karnatapu
+// Purpose : Write a C# Code, to copy from one folder to another folder, by scheduling the job using Task Schedular in Windows OS.
+
+// For Reference, Check Day15Project2 in the same Repository.
+namespace Day15Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\C#\GitHub\FileOperations.txt";
+
+            string TaskSchedularPath = @"D:\C#\TaskSchedular\TaskSchedularPath.txt";
+            if(File.Exists(TaskSchedularPath))
+                File.Delete(TaskSchedularPath);
+
+            File.Copy(filePath, TaskSchedularPath);
+
+            Console.WriteLine("File Copying is Done");
+
+            Console.WriteLine("\n Task Schedular Executed the Given Operation, Succesfully.");
+            Console.ReadKey();
+        }
+    }
+}
