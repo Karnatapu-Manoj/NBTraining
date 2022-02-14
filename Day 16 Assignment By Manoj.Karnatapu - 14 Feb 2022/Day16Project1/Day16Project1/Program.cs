6adedeb771da3ff8b851d58d3a546b39d1cdbed9
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-14 20:56:01 +0530

    Printing Hello World, Using Object Oriented Approach

---
+++ Day 16 Assignment By Manoj.Karnatapu - 14 Feb 2022/Day16Project1/Day16Project1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Write C# Code to Print Hello World, Using Object Oriented Approach
+
+// For Reference, Check Day16Project1 in the same Repositpory
+
+namespace Day16Project1
+{
+    class HelloWorld
+    {
+        /// <summary>
+        /// Displaying the Hello World Message
+        /// </summary>
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello World...!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            helloWorld.PrintMessage();
+        }
+    }
+}
