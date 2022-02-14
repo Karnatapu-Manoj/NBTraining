9c5d008f961ba6add36d126eb47e3d41af17e8e8
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-14 21:08:51 +0530

    Class Libraries implemented into ConsoleApp & WindowsDesktopFormApp

---
+++ Day 16 Assignment By Manoj.Karnatapu - 14 Feb 2022/MyProject/DesktopAppFactorial/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using ManojLibrary;
+using PublicLibrary;
+
+// Author : Manoj.Karnatapu
+// Purpose : Creating desktop App, using ManojLibray.
+
+// for Reference, check MyProject>DesktopAppFactorial. in the same Repository.
+namespace DesktopAppFactorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input = int.Parse(textBox1.Text);
+            int factorial = Mathematics.Factorial(input);
+
+            textBox2.Text = factorial.ToString();
+        }
+    }
+}
---
+++ Day 16 Assignment By Manoj.Karnatapu - 14 Feb 2022/MyProject/PublicLibrary/Physics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : This is a Mathematics Class in <ManojLibrary>
+
+// for Reference, Check Physics.cs in PublicLibrary inside MyProject Solution.
+
+namespace PublicLibrary
+{
+    public class Physics
+    {
+        /// <summary>
+        /// This is a Final Velocity Calculation
+        /// </summary>
+        /// <param name="u">initial velocity</param>
+        /// <param name="a">acceleration</param>
+        /// <param name="t">time</param>
+        /// <returns>Final Velocity</returns>
+        public static int FinalVelocity(int u, int a, int t)
+        {
+            int finalVelocity = u + a * t;
+            Console.WriteLine(finalVelocity);
+            return finalVelocity;
+        }
+        /// <summary>
+        /// This is a Force Calculation Method
+        /// </summary>
+        /// <param name="m">Mass</param>
+        /// <param name="a">Acceleration</param>
+        /// <returns>Force</returns>
+        public static int ForceCalculation(int m, int a)
+        {
+            int force = m * a;
+            Console.WriteLine(force);
+            return force;
+        }
+    }
+
+}
