5f075c1582135f35f2863418843dddb584ac082c
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-14 21:04:12 +0530

    Using Partial Classes inside ClassLibrary & using in ConsoleApp

---
+++ Day 16 Assignment By Manoj.Karnatapu - 14 Feb 2022/Day16Project5/Day16Project5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ManojLibrary;
+
+// Author : Manoj.Karnatapu
+// Purpose : Creating a Mathematics public class for Reusability Using Partial Class in ManojLibrary.
+
+// For Reference, Check ManojLibrary in Day16Project5 in the same Repository.
+namespace Day16Project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Executing Mathematical Operations Using Partial Class \n");
+
+            Console.Write("\nAddition of 5 & 6 is : ");
+            Mathematics.Addition(5, 6);
+            Console.Write("\nSubtraction of 7 & 3 is : ");
+            Mathematics.Subtraction(7, 3);
+            Console.Write("\nMultiplication of 8 & 4 is : ");
+            Mathematics.Multiplication(8, 4);
+            Console.Write("\nDivision of 9 / 5 is : ");
+            Mathematics.Division(9, 5);
+
+            Console.WriteLine("\n Factorial of 5 is : {0}",Mathematics.Factorial(5));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day 16 Assignment By Manoj.Karnatapu - 14 Feb 2022/Day16Project5/ManojLibrary/Mathematics2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Author : Manoj.Karnatapu
+// Purpose : Creating a Mathematics public class for Reusability Using Partial Class in ManojLibrary.
+
+// For Reference, Check ManojLibrary in Day16Project5 in the same Repository.
+namespace ManojLibrary
+{
+    public static partial class Mathematics
+    {
+        public static int Factorial(int input)
+        {
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
