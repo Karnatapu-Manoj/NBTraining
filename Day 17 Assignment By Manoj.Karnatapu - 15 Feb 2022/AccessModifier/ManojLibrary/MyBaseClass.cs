6c0fde08cf2a80df280bd50ca7e4b23a8c29d970
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-15 12:24:56 +0530

    Checking Access Modifiers in Different Locations.

---
+++ Day 17 Assignment By Manoj.Karnatapu - 15 Feb 2022/AccessModifier/ManojLibrary/MyBaseClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+// Author : Manoj.Karnatapu
+// Purpose : Created a Manoj Library for identifying the Access Modifiers.
+
+// For Reference, check AccessModifier > Manoj Library > MyBaseClass.cs in the same Repository.
+
+namespace ManojLibrary
+{
+    /// <summary>
+    /// This is a Base Class Initializing with Access modifier variables.
+    /// </summary>
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        /// <summary>
+        /// This is a BaseClassMethod(), Checking wether accessing the Variables is Possible or not
+        /// </summary>
+        public void MyBaseClassMethod()
+        {
+            a = 5;
+            b = 10;
+            c = 15;
+            d = 20;
+            e = 30;
+
+        }
+    }
+
+    /// <summary>
+    /// This is a DerivedClass of BaseClass,inheriting the properties of baseclass. 
+    /// </summary>
+    public class MyDerivedClass : MyBaseClass
+    {
+       public void MyDerivedClassMethod()
+        {
+            a = 5;
+            // b = 10;    // Here the Private Access Modifier is not accessed in the Derived Class
+            c = 15;
+            d = 20;
+            e = 30;
+        }
+    }
+    /// <summary>
+    /// This is a separate class, checking the accessed variables are using in outside of a class or not.
+    /// </summary>
+    public class MyOtherClass
+    {
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass mb = new MyBaseClass();
+            mb.a = 5;
+            // mb.b = 10; Here the Private Access Modifier is not accessed in the other Class in Same Assembly
+            // mb.c = 15; Here the Protected Access Modifier is not accessed in the other Class in Same Assembly
+            mb.d = 20;
+            mb.e = 30;
+
+        }
+    }
+}
---
+++ Day 17 Assignment By Manoj.Karnatapu - 15 Feb 2022/AccessModifier/PublicLibrary/MyPublicDerivedClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ManojLibrary;
+// Author : Manoj.Karnatapu
+// Purpose : Created a Public Library and Using ManojLibrary namespace for identifying the Access Modifiers.
+
+// For Reference, check AccessModifier > Public Library > MyPublicDerivedClass.cs in the same Repository.
+
+namespace PublicLibrary
+{
+    /// <summary>
+    /// This is a MyPublicDerivedClass in other Assembly by inheriting the MyBaseClass and identifying the Access Modifier accessability.
+    /// </summary>
+    public  class MyPublicDerivedClass : MyBaseClass
+    {
+        /// <summary>
+        /// MyPublicDerivedClassMethod(), we are using the variables from Inherited MyBaseClass of ManojLibrary & identifying the Access Modifier accessability
+        /// </summary>
+        public void MyPublicDerivedClassMethod()
+        {
+            a = 5;
+            //b = 10;    // Here the Private Access Modifier is not accessed in the Derived Class in Other Assembly
+            c = 15;
+            //d = 20;    // Here the internal Access Modifier is not accessed in the Derived Class in Other Assembly
+            e = 30;
+        }
+    }
+
+    /// <summary>
+    /// This is a MyPublicOtherClass in Other Assembly & Identifying the Access Modifier accessability.
+    /// </summary>
+    public class MyPublicOtherClass
+    {
+        /// <summary>
+        /// MyPublicOtherClassMethod(), we are creating the object for MyPublicDerivedClass & identifying the Access Modifier accessability
+        /// </summary>
+        public void MyPublicOtherClassMethod()
+        {
+            MyPublicDerivedClass mpd = new MyPublicDerivedClass();
+            mpd.a = 5;
+            // mpd.b = 10;  Here the Private Access Modifier is not accessed in the other Class in Other Assembly
+            // mpd.c = 15;  Here the Protected Access Modifier is not accessed in the other Class in Other Assembly
+            // mpd.d = 20;  Here the internal Access Modifier is not accessed in the other Class in Other Assembly
+            // mpd.e = 30;  Here the Protected Internal Access Modifier is not accessed in the other Class in Other Assembly
+        }
+    }
+}
