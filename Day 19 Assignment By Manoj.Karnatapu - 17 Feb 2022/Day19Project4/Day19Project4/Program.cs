33cb0fa89f48c824c6620c59e619465ff45fb823
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-17 20:11:37 +0530

    Reading ID & printing its product name, from XML in C#

---
+++ Day 19 Assignment By Manoj.Karnatapu - 17 Feb 2022/Day19Project4/Day19Project4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+
+// Author : Manoj.Karnatapu
+// Purpose : C# Code To read Xml Data from a Xml file & printing the data name for the User Given ID.
+
+// For Reference, Check Day19Project4 in the same Repository.
+
+namespace Day19Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter Any ID Number from 1- 10, To find the Product : ");
+            int input = int.Parse(Console.ReadLine());
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"D:\C#\GitHub\Day19Project4\AttributeBasedXML.xml");
+            var Mobile = doc.GetElementsByTagName("Mobile")[input - 1].Attributes["Name"].Value;
+
+            Console.WriteLine(Mobile);   
+
+            Console.ReadLine();
+        }
+    }
+}
