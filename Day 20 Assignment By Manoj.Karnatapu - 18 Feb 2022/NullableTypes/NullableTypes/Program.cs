84bc4e382388021c9fbd2fc0af024219aee83255
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-02-20 07:29:20 +0530

    Nullable types C# Code Example

---
+++ Day 20 Assignment By Manoj.Karnatapu - 18 Feb 2022/NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// Author : Manoj.Karnatapu
+// Purpose : Nullable Type C# Code Example.
+
+// For reference, check NullableTypes project in the same reference.
+
+namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int? firstValue = 20;
+            int? secondValue = null;
+
+            int? result;
+
+            result = (firstValue.HasValue) ? firstValue : null;
+
+            Console.WriteLine("The value inside First Value Nullable type is : {0}", result);
+
+            result = (secondValue.HasValue) ? secondValue : null;
+
+            Console.WriteLine("The value inside Second Value Nullable type is : {0}", result);
+            // If u find No Output displaying in the console window means, it's value is null i.e., unassigned.
+
+            Console.ReadKey();
+        }
+    }
+}
